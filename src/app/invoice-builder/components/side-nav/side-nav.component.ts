0a51881eafd37eaabfedcaf1b9d43a36c39b3d8e
amit9031jsr <amit9031jsr@gmail.com>
2020-10-06 15:02:31 +0530

    added responsiveness to the sidenav

--- src/app/invoice-builder/components/side-nav/side-nav.component.ts
+++ src/app/invoice-builder/components/side-nav/side-nav.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgZone, OnInit } from '@angular/core';
 
+const MAX_WIDTH_BREAKPOINT = 720;
 @Component({
   selector: 'app-side-nav',
   templateUrl: './side-nav.component.html',
   styleUrls: ['./side-nav.component.scss']
 })
 export class SideNavComponent implements OnInit {
-  shouldRun = [/(^|\.)plnkr\.co$/, /(^|\.)stackblitz\.io$/].some(h => h.test(window.location.host));
-  constructor() { }
+
+  private mediaMatcher: MediaQueryList = 
+  matchMedia(`(max-width: ${MAX_WIDTH_BREAKPOINT}px)`)
+  
+  constructor(zone: NgZone) { 
+    this.mediaMatcher.addEventListener('change', (mql) => {
+      zone.run(() => mql)
+    })
+  }
 
   ngOnInit(): void {
+  }
+
+  isScreenSmall() {
+    return this.mediaMatcher.matches;
   }
 
 }
